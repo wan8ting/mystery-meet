57e700963186176f4a96901bdbf34451114b3fc3
wan8ting <wan8ting@gmail.com>
2025-08-29 02:09:47 +0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwSQtQM16W-1FQ4NN1dWaLKjsRx_2W41U",
+  authDomain: "mystery-meet.firebaseapp.com",
+  projectId: "mystery-meet",
+  storageBucket: "mystery-meet.firebasestorage.app",
+  messagingSenderId: "648529916541",
+  appId: "1:648529916541:web:3c02a7bfa827c32d2b3714",
+  measurementId: "G-8KWV1RN1BP"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
